342d2c8db1cb9e8260b0f6c0ce47de28499e108c
lianginger <jackyliang444@hotmail.com.tw>
2019-04-28 16:34:43 +0800

    Add Mongoose connection and create Record and User model

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+// 設定連線到 mongoDB
+// 開發時可以打開 mongoose.set("debug", true)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/expense_tracker', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongodb is connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello')
---
+++ models/record.js
@@ -0,0 +1,36 @@
+// 支出 model：Record
+// 支出名稱：name
+// 類別：category
+// 日期：date
+// 金額：amount
+// 在首頁看到的總金額：totalAmount
+// 擁有這個支出的使用者 ID：userId
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
