5dbd404a3ba48cc76185f0dbdd378cbf58348be7
lianginger <jackyliang444@hotmail.com.tw>
2019-04-28 22:48:30 +0800

    Add user registeration feature

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // 登入頁面
 router.get('/login', (req, res) => {
@@ -18,7 +21,43 @@ router.get('/register', (req, res) => {
 
 // 註冊檢查
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, password2 } = req.body
+  // 加入錯誤訊息提示
+  let errors = []
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if (password !== password2) {
+    errors.push({ message: '密碼輸入錯誤' })
+  }
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    })
+  } else {
+    User.findOne({ email }).then(user => {
+      if (user) {
+        errors.push({ message: '這個 Email 已經註冊過了' })
+        res.render('register', { errors, name, email, password, password2 })
+      } else {
+        const newUser = new User(req.body)
+        bcrypt.genSalt(10, function (err, salt) {
+          bcrypt.hash(newUser.password, salt, function (err, hash) {
+            if (err) throw err
+            // Store hash in your password DB.
+            newUser.password = hash
+            newUser.save()
+              .then(user => { res.redirect('/') })
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+  }
 })
 
 // 登出
