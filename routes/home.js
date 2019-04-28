a56c63e20cf42b1e7c4cb49db8bfd4937b07eb49
lianginger <jackyliang444@hotmail.com.tw>
2019-04-29 00:58:59 +0800

    Add filter feature on index page

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,15 @@ const router = express.Router()
 const Record = require('../models/record')
 
 router.get('/', (req, res) => {
-  Record.find({ userId: req.user._id })
+  const filterMonth = req.query.filterMonth || ''
+  const filterMonthRegExp = new RegExp('2019-' + filterMonth, 'i')
+  const filterCategory = req.query.filterCategory || ''
+  const filterCategoryRegExp = new RegExp(filterCategory, 'i')
+  Record.find({
+    userId: req.user._id,
+    date: { $regex: filterMonthRegExp },
+    category: { $regex: filterCategoryRegExp }
+  })
     .sort()
     .exec((err, records) => {
       if (err) return console.log(err)
@@ -12,7 +20,7 @@ router.get('/', (req, res) => {
         totalAmount = records.map(item => parseInt(item.amount))
           .reduce((accumulator, currentItem) => accumulator + currentItem)
       }
-      res.render('index', { records, totalAmount })
+      res.render('index', { records, totalAmount, filterMonth, filterCategory })
     })
 })
 
