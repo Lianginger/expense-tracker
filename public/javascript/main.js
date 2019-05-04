aa8b0473cd7a060bf926e695058f50906c29c83d
lianginger <jackyliang444@hotmail.com.tw>
2019-05-05 00:51:51 +0800

    Add image upload format checker

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,6 +5,17 @@ $("#user-image").change(function () {
 
 function checkImageUpload(input) {
   if (input.files && input.files[0]) {
-    $("#user-img-layer").text('上傳成功！')
+    const uploadFileName = input.value
+    const uploadFileFormat = uploadFileName.substring(uploadFileName.lastIndexOf('.'))
+    const validFileFormat = ['.jpg', '.png', '.jpeg']
+
+    if (validFileFormat.indexOf(uploadFileFormat) < 0) {
+      alert('上傳檔案類型錯誤，請上傳 jpg 或 png 檔喲！')
+      input.value = null
+      $("#user-img-layer").text('請重新上傳！')
+      return false
+    } else {
+      $("#user-img-layer").text('上傳成功！')
+    }
   }
 }
