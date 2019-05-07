539f8dcd26ab90cefa5f453eeedca26af479d331
lianginger <jackyliang444@hotmail.com.tw>
2019-05-08 00:26:20 +0800

    Add monthly spend chart

--- routes/statistic.js
+++ routes/statistic.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/user')
+const Record = require('../models/record')
 
 router.get('/', (req, res) => {
-  res.render('statistic')
+  Record.find({ userId: req.user._id })
+    .then(records => {
+      const monthlySpend = []
+      for (let i = 0; i < 12; i++) {
+        monthlySpend[i] = records.filter(record => {
+          return parseInt(record.date.slice(5, 7)) === (i + 1)
+        }).reduce((total, item) => {
+          return total + parseInt(item.amount)
+        }, 0)
+      }
+      res.render('statistic', { monthlySpend })
+      console.log(monthlySpend)
+    })
+
 })
 
 module.exports = router
