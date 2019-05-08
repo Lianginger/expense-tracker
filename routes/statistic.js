d5fabdec39ac09d70660a3b99f758c15b2134e5f
lianginger <jackyliang444@hotmail.com.tw>
2019-05-08 23:57:56 +0800

    Add pie chart for category spend

--- routes/statistic.js
+++ routes/statistic.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../models/record')
+const filterData = require('../public/data/filterData.json')
+const category = filterData.category
 
 router.get('/', (req, res) => {
   Record.find({ userId: req.user._id })
@@ -13,8 +15,26 @@ router.get('/', (req, res) => {
           return total + parseInt(item.amount)
         }, 0)
       }
-      res.render('statistic', { monthlySpend })
-      console.log(monthlySpend)
+
+      const categorySpend = {}
+      const categorySpendArray = []
+      let categorySpendData = ''
+      let totalSpend = 0
+      records.map(record => {
+        if (categorySpend[record.category]) {
+          categorySpend[record.category] += parseInt(record.amount)
+        } else {
+          categorySpend[record.category] = parseInt(record.amount)
+        }
+        totalSpend += parseInt(record.amount)
+      })
+      for (let key in categorySpend) {
+        categorySpendArray.push(
+          [category[key], 100 * categorySpend[key] / totalSpend])
+      }
+      categorySpendData = JSON.stringify(categorySpendArray)
+      console.log(categorySpendData)
+      res.render('statistic', { monthlySpend, categorySpendData, totalSpend })
     })
 
 })
